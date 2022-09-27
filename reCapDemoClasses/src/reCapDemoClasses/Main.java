a0cc838a98a64342defb559595973f644ec93cd2
safayilmaz56 <muhammedsafayilmaz56@gmail.com>
2022-09-27 07:43:04 +0300

    reCapDemoClasses

---
+++ reCapDemoClasses/src/reCapDemoClasses/Main.java
@@ -0,0 +1,14 @@
+package reCapDemoClasses;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		DortISlem dortISlem = new DortISlem();
+		System.out.println(dortISlem.topla(3,4));
+		System.out.println(dortISlem.cikar(9,5));
+		System.out.println(dortISlem.carp(3,4));
+		System.out.println(dortISlem.bol(12,3));
+	}
+
+}
